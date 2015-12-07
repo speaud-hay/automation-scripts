f39f8fdfa322387f93ef1fe690fa6422747c5c3c
Josh Mummert Stern <josh.mummertstern@haygroup.com>
2015-12-07 15:57:58 -0500

    set up struct for deployment automation

---
+++ deployment/gitToTest1.sh
@@ -0,0 +1,16 @@
+<<README
+
+@title 				Deployment Automation
+@version			1.0
+@author 			Speaud
+@description		...
+
+README
+
+#!/bin/bash
+clear
+
+printf "\e[1;37;44mLocal Environment Setup Automation v2.0\t\n---------------------------------------\t\n@author\t\tSpeaud\t\t\t\n@license\tThe MIT License (MIT)\t\n\n\033[0m"
+
+# script instruction indicator
+sii="\e[1;37;44m >> \033[0m"
